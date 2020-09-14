fdd93e0dc1936fe8071fcbf997882a32bfe8ba5b
qweroot <qweroot@pm.me>
2020-09-14 09:06:44 +0900

    delete: unused scripts

--- bulk_dirsearch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while read subdomain
-do
-	host=$(echo $subdomain | unfurl domains)
-	dirsearch -u $subdomain -E --plain-text-report=$host.txt	
-done < $1
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for dir in /home/qweroot/hacking/data/nuclei-templates/*/
-do
-	echo $dir
-	template=$(basename $dir)
-	echo $template
-done 
